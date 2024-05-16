91753cbb8fd837f37457373bd3e1172b3bbe1273
Rafayel Mkrtchyan <rafayelmkrtchyan12@gmail.com>
2024-05-16 12:01:26 +0400

    Training tested and fixed

--- algorithm.py
+++ algorithm.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from typing import Any
 
 import pytorch_lightning as pl
@@ -5,7 +6,7 @@ import torch
 from pytorch_lightning.utilities.types import OptimizerLRScheduler, STEP_OUTPUT
 from torch import nn
 from torch.optim import Adam
-from torcheval.metrics.functional import multiclass_f1_score
+from torchmetrics import Accuracy, F1Score
 
 
 class FruitDetector(pl.LightningModule):
@@ -18,7 +19,14 @@ class FruitDetector(pl.LightningModule):
         super().__init__()
         self._num_classes = num_classes
         self._network = network
+        
         self._loss = nn.CrossEntropyLoss()
+        self._f1 = F1Score(task="multiclass", num_classes=self._num_classes, average="macro")
+        self._accuracy = Accuracy(task="multiclass", num_classes=self._num_classes)
+        
+        self._training_step_outputs = defaultdict(list)
+        self._validation_step_outputs = defaultdict(list)
+        self._test_step_outputs = defaultdict(list)
     
     @property
     def network(self):
@@ -30,19 +38,69 @@ class FruitDetector(pl.LightningModule):
     def configure_optimizers(self) -> OptimizerLRScheduler:
         return Adam(filter(lambda p: p.requires_grad, self.parameters()))
     
-    def _step(self, batch):
+    def _step(self, batch, split: str):
         input_image, label = batch
         pred = self._network(input_image)
         loss = self._loss(pred, nn.functional.one_hot(label, num_classes=self._num_classes).to(torch.float16))
-        f1 = multiclass_f1_score(pred, label, average="macro", num_classes=self._num_classes)
+        f1 = self._f1(pred, label)
+        accuracy = self._accuracy(pred, label)
         
-        return dict(loss=loss, f1=f1)
+        self.log(f"loss", loss, prog_bar=True)
+        self.log(f"f1", f1, prog_bar=True)
+        self.log(f"accuracy", accuracy, prog_bar=True)
+        
+        return dict(loss=loss, f1=f1, accuracy=accuracy)
     
     def training_step(self, batch, *args: Any, **kwargs: Any) -> STEP_OUTPUT:
-        return self._step(batch)
+        return self._step(batch, split="train")
     
     def validation_step(self, batch, *args: Any, **kwargs: Any) -> STEP_OUTPUT:
-        return self._step(batch)
+        return self._step(batch, split="val")
     
     def test_step(self, batch, *args: Any, **kwargs: Any) -> STEP_OUTPUT:
-        return self._step(batch)
+        return self._step(batch, split="test")
+    
+    def on_train_batch_end(self, outputs: STEP_OUTPUT, batch: Any, batch_idx: int) -> None:
+        for metric_name, metric_values in outputs.items():
+            self._training_step_outputs[metric_name].append(metric_values.detach().cpu())
+    
+    def on_validation_batch_end(
+        self, outputs: STEP_OUTPUT, batch: Any, batch_idx: int, dataloader_idx: int = 0
+    ) -> None:
+        for metric_name, metric_values in outputs.items():
+            self._validation_step_outputs[metric_name].append(metric_values.detach().cpu())
+    
+    def on_test_batch_end(self, outputs: STEP_OUTPUT, batch: Any, batch_idx: int, dataloader_idx: int = 0) -> None:
+        for metric_name, metric_values in outputs.items():
+            self._test_step_outputs[metric_name].append(metric_values.detach().cpu())
+    
+    @staticmethod
+    def _calculate_epoch_metrics(outputs: dict[str, list]) -> dict:
+        epoch_metrics = {}
+        
+        for metric_name, metric_values in outputs.items():
+            epoch_metrics[metric_name] = sum(metric_values) / len(metric_values)
+        
+        return epoch_metrics
+    
+    def _epoch_end(self, outputs, split):
+        epoch_metrics = self._calculate_epoch_metrics(outputs)
+        epoch_metrics = {f'{split}_{k}': v for k, v in epoch_metrics.items()}
+        
+        self.trainer.callback_metrics.update(epoch_metrics)
+        if self.logger:
+            self.logger.log_metrics(epoch_metrics, self.trainer.current_epoch)
+        else:
+            print(f"""\n{epoch_metrics}\n""")
+    
+    def on_train_epoch_end(self) -> None:
+        self._epoch_end(self._training_step_outputs, split='train')
+        self._training_step_outputs.clear()
+    
+    def on_validation_epoch_end(self) -> None:
+        self._epoch_end(self._training_step_outputs, split='val')
+        self._training_step_outputs.clear()
+    
+    def on_test_epoch_end(self) -> None:
+        self._epoch_end(self._training_step_outputs, split='test')
+        self._training_step_outputs.clear()
--- train.py
+++ train.py
@@ -1,21 +1,26 @@
+from datetime import datetime
+
 import click
 import torch.nn as nn
 import torchvision.models as models
 from pytorch_lightning import Trainer
+from pytorch_lightning.callbacks import ModelCheckpoint, RichProgressBar
+from pytorch_lightning.loggers import TensorBoardLogger
+from torchvision.models import ResNet18_Weights
 
 from algorithm import FruitDetector
 from datamodule import FruitsDatamodule
 
 
 def get_network(num_classes: int):
-    resnet50 = models.resnet50(pretrained=True)
+    resnet18 = models.resnet18(weights=ResNet18_Weights.IMAGENET1K_V1)
     
-    for param in resnet50.parameters():
+    for param in resnet18.parameters():
         param.requires_grad = False
     
-    resnet50.fc = nn.Linear(resnet50.fc.in_features, num_classes)
+    resnet18.fc = nn.Linear(resnet18.fc.in_features, num_classes)
     
-    return resnet50
+    return resnet18
 
 
 @click.command()
@@ -24,13 +29,39 @@ def get_network(num_classes: int):
 @click.option("--batch_size", default=8)
 @click.option("--num_workers", default=8)
 @click.option("--num_epochs", default=5)
-@click.option("--precision", default=16)
-def train(data_dir: str, num_classes: int, batch_size: int, num_workers: int, num_epochs: int, precision: int):
+def train(data_dir: str, num_classes: int, batch_size: int, num_workers: int, num_epochs: int):
+    print("Creating the network")
     network = get_network(num_classes=num_classes)
+    print("Creating the datamodule")
     datamodule = FruitsDatamodule(batch_size=batch_size, num_workers=num_workers, data_dir=data_dir)
+    print("Creating the algorithm")
     algorithm = FruitDetector(num_classes=num_classes, network=network)
-    trainer = Trainer(max_epochs=num_epochs, precision=precision)
+    print("Creating model checkpoint callback")
+    now = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    model_checkpoint = ModelCheckpoint(
+        monitor="val_loss",
+        mode="min",
+        save_top_k=3,
+        save_last=True,
+        dirpath=f"../checkpoints/{now}",
+        filename="epoch_{epoch:02d}",
+        auto_insert_metric_name=False
+    )
+    print("Creating the logger")
+    logger = TensorBoardLogger(f"../logs/{now}")
+    print("Creating the progress bar")
+    progress_bar = RichProgressBar()
+    print("Creating the trainer")
+    trainer = Trainer(
+        max_epochs=num_epochs,
+        callbacks=[model_checkpoint, progress_bar],
+        log_every_n_steps=1,
+        logger=logger
+    )
+    print("Starting the training")
     trainer.fit(algorithm, datamodule=datamodule)
+    print("Testing")
+    trainer.test(dataloaders=datamodule.test_dataloader(), ckpt_path="best")
 
 
 if __name__ == "__main__":
