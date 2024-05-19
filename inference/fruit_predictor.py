0d0453ee0d53fef81ee5d361dd1c0c165bb71999
Norayr Baghdasaryan <norayrbaghdasaryann@gmail.com>
2024-05-20 02:49:06 +0400

    Change methods order

--- inference/fruit_predictor.py
+++ inference/fruit_predictor.py
@@ -12,6 +12,47 @@ from preprocessing import process_image
 
 
 class FruitPredictor:
+    def __init__(self):
+        config = FruitPredictor.__read_config()
+        # Store image size for preprocessing
+        self.image_size = config["preprocess_config"]['image_size']
+        # Extract number of classes from config file
+        num_classes = config["train_config"]['num_classes']
+        # Load model and set model attribute
+        self.__load_model(num_classes)
+        # Load and create index to fruit mapping
+        self.__load_index_to_fruit_mapping()
+
+    def predict_from_paths(self, image_paths):
+        """
+        Predicts fruits in the images determined by passed image_paths list
+        :param image_paths: Iterable object consisting valid image paths
+        :return: predicted fruits list
+        """
+        outputs = self.__model_outputs_from_paths(image_paths)
+        _, predicted = torch.max(outputs, 1)
+
+        return self.__index_to_fruit(predicted)
+
+    def predict_from_path(self, image_path):
+        """
+        Predicts fruits in the images determined by passed image_paths list
+        :param image_path: Valid image path
+        :return: predicted fruit
+        """
+        return self.predict_from_paths([image_path])[0]
+
+    def predict_from_tensor(self, tensor):
+        """
+        Predicts fruits in the images determined by passed images tensor
+        :param tensor: Expects 4-dimensional tensor with following structure: (batch_size, channels, width, height)
+        :return: predicted fruits list
+        """
+        outputs = self.__model_outputs_from_tensor(tensor)
+        _, predicted = torch.max(outputs, 1)
+
+        return self.__index_to_fruit(predicted)
+
     @staticmethod
     def __read_config():
         with open(os.path.join(root, "config.json"), "r") as config:
@@ -36,17 +77,6 @@ class FruitPredictor:
             for k, v in reverse_mapping.items():
                 self.index_to_fruit_mapping[v] = k
 
-    def __init__(self):
-        config = FruitPredictor.__read_config()
-        # Store image size for preprocessing
-        self.image_size = config["preprocess_config"]['image_size']
-        # Extract number of classes from config file
-        num_classes = config["train_config"]['num_classes']
-        # Load model and set model attribute
-        self.__load_model(num_classes)
-        # Load and create index to fruit mapping
-        self.__load_index_to_fruit_mapping()
-
     def __index_to_fruit(self, tensor):
         return list(map(lambda index: self.index_to_fruit_mapping[int(index)], tensor))
 
@@ -67,33 +97,3 @@ class FruitPredictor:
         # Concat image tensors into batch tensor
         batch_tensor = torch.cat([self.__preprocess_image(path) for path in image_paths], dim=0)
         return self.__model_outputs_from_tensor(batch_tensor)
-
-    def predict_from_paths(self, image_paths):
-        """
-        Predicts fruits in the images determined by passed image_paths list
-        :param image_paths: Iterable object consisting valid image paths
-        :return: predicted fruits list
-        """
-        outputs = self.__model_outputs_from_paths(image_paths)
-        _, predicted = torch.max(outputs, 1)
-
-        return self.__index_to_fruit(predicted)
-
-    def predict_from_path(self, image_path):
-        """
-        Predicts fruits in the images determined by passed image_paths list
-        :param image_path: Valid image path 
-        :return: predicted fruit
-        """
-        return self.predict_from_paths([image_path])[0]
-
-    def predict_from_tensor(self, tensor):
-        """
-        Predicts fruits in the images determined by passed images tensor
-        :param tensor: Expects 4-dimensional tensor with following structure: (batch_size, channels, width, height)
-        :return: predicted fruits list
-        """
-        outputs = self.__model_outputs_from_tensor(tensor)
-        _, predicted = torch.max(outputs, 1)
-
-        return self.__index_to_fruit(predicted)
