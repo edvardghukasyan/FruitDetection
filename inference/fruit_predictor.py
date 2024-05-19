bf66dc0f2ed60b812e5b933fb47dddb04109a006
Norayr Baghdasaryan <norayrbaghdasaryann@gmail.com>
2024-05-19 18:29:11 +0400

    Implement Predictor class

--- inference/fruit_predictor.py
+++ inference/fruit_predictor.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import json
 import torch
 from torchvision import transforms
 from PIL import Image
@@ -7,26 +8,43 @@ from PIL import Image
 sys.path.append(os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
 from training import FruitDetector, get_network
 
-# Get the Conda environment root directory
-root = os.path.dirname(os.path.dirname(__file__))
-checkpoint_path = os.path.join(root, 'model', 'fruit_detection_model.ckpt')
 
-model = FruitDetector.load_from_checkpoint(checkpoint_path, num_classes=7, network=get_network(7))
-model.eval()
+class FruitPredictor:
+    def __init__(self):
+        # Get the Conda environment root directory
+        root = os.path.dirname(os.path.dirname(__file__))
+        checkpoint_path = os.path.join(root, 'model', 'fruit_detection_model.ckpt')
+        with open(os.path.join(os.path.dirname(os.path.dirname(__file__)), "config.json"), "r") as config:
+            # Extract number of classes from config file
+            config = json.load(config)["train_config"]
+            num_classes = config['num_classes']
+            # Get the network
+            network = get_network(num_classes)
+            # Load checkpoint into model
+            self.model = FruitDetector.load_from_checkpoint(checkpoint_path, num_classes=num_classes, network=network)
+        # Set the model to evaluation mode
+        self.model.eval()
 
-# Define the image transformations
-preprocess = transforms.Compose([
-    transforms.ToTensor()
-])
+    def predict_proba(self, image_path):
+        # Define the image transformations
+        preprocess = transforms.Compose([
+            transforms.ToTensor()
+        ])
 
-# Load and preprocess the image
-image_path = '../fruits360_processed/Test/apple/6r0_3.jpg'
-image = preprocess(Image.open(image_path)).unsqueeze(0)
+        # Load and preprocess the image
+        image = preprocess(Image.open(image_path)).unsqueeze(0)
 
-# Make predictions
-with torch.no_grad():
-    outputs = model.network(image)
-    _, predicted = torch.max(outputs, 1)
-    print(outputs)
+        # Make predictions
+        with torch.no_grad():
+            return self.model.network(image)
 
-print(f'Predicted class: {predicted.item()}')
+    def predict(self, image_path):
+        outputs = self.predict_proba(image_path)
+        _, predicted = torch.max(outputs, 1)
+        print(predicted)
+
+        return predicted
+
+
+fruitPredictor = FruitPredictor()
+print('Prediction:', fruitPredictor.predict('../fruits360_processed/Test/apple/6r0_3.jpg'))
