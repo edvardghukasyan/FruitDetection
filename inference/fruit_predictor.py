ba0077e4a4cedc83e54a0c90dbf1805f18fb9297
Norayr Baghdasaryan <norayrbaghdasaryann@gmail.com>
2024-05-20 01:20:54 +0400

    Add some structure to predictor class

--- inference/fruit_predictor.py
+++ inference/fruit_predictor.py
@@ -2,47 +2,54 @@ import sys
 import os
 import json
 import torch
-import torch.nn.functional as F
 from skimage import io
 
-sys.path.append(os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
+root = os.path.dirname(os.path.dirname(__file__))
+sys.path.append(os.path.abspath(root))
 from training import FruitDetector, get_network
 from preprocessing import process_image
 
 
 class FruitPredictor:
-    def __init__(self):
-        # Get the Conda environment root directory
-        root = os.path.dirname(os.path.dirname(__file__))
+    @staticmethod
+    def __read_config():
+        with open(os.path.join(root, "config.json"), "r") as config:
+            return json.load(config)
+
+    def __load_model(self, num_classes):
         checkpoint_path = os.path.join(root, 'model', 'fruit_detection_model.ckpt')
-        with open(os.path.join(os.path.dirname(os.path.dirname(__file__)), "config.json"), "r") as config:
-            # Extract number of classes from config file
-            config = json.load(config)
-            num_classes = config["train_config"]['num_classes']
-            # Get the network
-            network = get_network(num_classes)
-            # Load checkpoint into model
-            self.model = FruitDetector.load_from_checkpoint(checkpoint_path, num_classes=num_classes, network=network)
-            self.image_size = config["preprocess_config"]['image_size']
+        # Get the network
+        network = get_network(num_classes)
+        # Load checkpoint into model
+        self.model = FruitDetector.load_from_checkpoint(checkpoint_path, num_classes=num_classes, network=network)
         # Set the model to evaluation mode
         self.model.eval()
 
-    def __read_image_tensor(self, image_path):
+    def __init__(self):
+        config = FruitPredictor.__read_config()
+        # Store image size for preprocessing
+        self.image_size = config["preprocess_config"]['image_size']
+        # Extract number of classes from config file
+        num_classes = config["train_config"]['num_classes']
+        # Load model and set model attribute
+        self.__load_model(num_classes)
+
+    def __process_image(self, image_path):
         image = process_image(io.imread(image_path), image_size=self.image_size)
         torch_image = torch.from_numpy(image)
         # Torch requires float and other permutation of dimensions
-        return torch.permute(torch_image, (2, 0, 1)).float()
+        torch_image = torch.permute(torch_image, (2, 0, 1)).float()
+        # Torch also requires batch size, so add one additional dimension
+        return torch.unsqueeze(torch_image, 0)
 
-    def predict_proba(self, image_path):
-        image = self.__read_image_tensor(image_path)
-        image = torch.unsqueeze(image, 0)
-
+    def __model_outputs(self, image_path):
+        image = self.__process_image(image_path)
         # Make predictions
         with torch.no_grad():
             return self.model.network(image)
 
     def predict(self, image_path):
-        outputs = self.predict_proba(image_path)
+        outputs = self.__model_outputs(image_path)
         _, predicted = torch.max(outputs, 1)
 
         return predicted
